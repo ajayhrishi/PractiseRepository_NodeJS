8434628251e02ef9025c0d0f4b19d26d405a9c13
Ajay Hrishi V <ajayhrishi@gmail.com>
2023-06-22 17:59:40 +0530

    made files system in place to add the promises and then to test it

--- PromiseTestings/dogName.js
+++ PromiseTestings/dogName.js
@@ -1,14 +1,19 @@
 
-
+const fs = require('fs');
 //module.export = dogs[random()*11];
 
 function SelectDog(){
-    const dogs = [`German Shepherd`,`Bulldog`,`Labrador Retriever`,`French Bulldog`,`Siberian Husky`,`Afghan Hound`,`Dachshund`,`Border Collie`,`Chihuahua`,`Poodle`,`Alaskan Malamute`];
+    console.log('Arrived the SelectDog.js file');
+    let dogs = [`brabancon`,`mountain`,`lakeland`,`sharpei`,`irish`,`shiba`,`komondor`,`lhasa`,`tervuren`,`shepherd`,`african`];
     const SelectedDog = dogs[Math.floor(Math.random()*11)];
-    console.log(SelectedDog, ' exported from the dogName.js file');
+    console.log(SelectedDog, 'Selected from the Dog Names array.');
+    fs.writeFile(`./PromiseTestings/dog.txt`,SelectedDog, (err)=>{ if(err)console.log(err);});
+    console.log('wrote the name to dog.txt file');
+    console.log('exporting the dog name to the main JS file promiseTest.js');
     return SelectedDog;
 }
 
 
 
 exports.module = SelectDog();
+
--- promiseTest.js
+++ promiseTest.js
@@ -1,7 +1,19 @@
 
-const pick = require(`./PromiseTestings/dogName.js`);
+const { error } = require('console');
+const pick = require(`./PromiseTestings/dogName.js`); // as of now, i am using it only to trigger the dogName.js file that is inside the PromiseTestings folder. if needed I can access the name of the dog by entering. pick.module.
 const fs = require('fs');
 const superagent = require('superagent');
 
+let name;
+const fileReaderSync = () =>{
+    fs.readFile('./PromiseTestings/dog.txt',(err,data)=>{
+    name = data.toString();
+    superagent.get(`https://dog.ceo/api/breed/${name}/images/random`).then(res=>{console.log(res.body.message)
+    fs.writeFile('./PromiseTestings/Link.txt',res.body.message,err=>{return console.log(err);})}).catch(err=>{if(err)return console.log(err)});
+    });
+    
+}
 
-console.log(pick.module);
+fileReaderSync();
+
+
